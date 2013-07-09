e02cdbdf636dd97079212bcf70f581aa84c28e59
Rohit <rohitp.ror@gmail.com>
2013-07-09 18:03:02 +0530

    quick fix to email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -53,14 +53,16 @@ Cat::Application.configure do
   # config.action_mailer.raise_delivery_errors = false
   config.action_mailer.default_url_options = { :host => 'book-post.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
-
+  config.action_mailer.perform_deliveries = true
+  
   ActionMailer::Base.smtp_settings = {
-    :address        => "smtp.sendgrid.net",
-    :port           => "25",
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
     :authentication => :plain,
-    :user_name      => ENV["SENDGRID_USERNAME"],
-    :password       => ENV["SENDGRID_PASSWORD"],
-    :domain         => 'heroku.com'
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => 'heroku.com',
+    :enable_starttls_auto => true
   }
 
   # Enable threaded mode
