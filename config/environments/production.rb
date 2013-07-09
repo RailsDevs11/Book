16107e5d1e7daeb1b886a35783661cd9d1abdb86
Rohit <rohitp.ror@gmail.com>
2013-07-09 07:51:36 +0530

    quick fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,8 +58,8 @@ Book::Application.configure do
     :address        => "smtp.sendgrid.net",
     :port           => "25",
     :authentication => :plain,
-    :user_name      => ENV['SENDGRID_USERNAME'],
-    :password       => ENV['SENDGRID_PASSWORD']
+    :user_name      => ENV["SENDGRID_USERNAME"],
+    :password       => ENV["SENDGRID_PASSWORD"]
   }
 
   # Enable threaded mode
